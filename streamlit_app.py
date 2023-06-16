1ed66fd934545f20465495fe6ff2092659813393
Matt Stockmal <mestockmal@gmail.com>
2023-06-15 23:53:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+pip install langchain --upgrade
+pip3 install pinecone-client
+
 from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
