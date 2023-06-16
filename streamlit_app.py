d10ae4be5244fdcd8b78b1f2bced874658e4775f
Matt Stockmal <mestockmal@gmail.com>
2023-06-16 00:15:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ from langchain import OpenAI
 from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
-
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+PINECONE_API_ENV = st.secrets["PINECONE_API_ENV"]
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 # initialize pinecone
 pinecone.init(
