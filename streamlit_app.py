06f270b7f79367e7b1911013e41b8047894a5b4c
Matt Stockmal <mestockmal@gmail.com>
2023-06-16 01:00:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pinecone
 OPENAI_API_KEY = 'nokey'
 PINECONE_API_ENV = st.secrets["PINECONE_API_ENV"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
 # initialize pinecone
 pinecone.init(
     api_key=PINECONE_API_KEY,  # find at app.pinecone.io
@@ -17,8 +17,6 @@ index_name = "hinsdale-doings-try1" # put in the name of your pinecone index her
 docsearch = Pinecone.from_existing_index(index_name, embeddings)
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-chain = load_qa_chain(llm, chain_type="stuff")
 query = "Who was William Robbins? Please write 3 paragraphs explaining his impact on the development of Hinsdale"
 # docs = docsearch.similarity_search(query)
 # chain.run(input_documents=docs, question=query)
@@ -32,8 +30,11 @@ with st.sidebar:
    OPENAI_API_KEY = st.text_input('OpenAI API Key')
 
 def generate_response(input_text):
- docs = docsearch.similarity_search(input_text)
- chain.run(input_documents=docs, question=input_text)   
+    llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    chain = load_qa_chain(llm, chain_type="stuff")
+    docs = docsearch.similarity_search(input_text)
+    chain.run(input_documents=docs, question=input_text)   
   # llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   # st.info(llm(input_text))
 
