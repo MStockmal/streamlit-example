ed4301bf47a3bce1cc550960d31a6107b35b3e56
Matt Stockmal <mestockmal@gmail.com>
2023-06-15 23:23:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,17 @@
-import openai
 import streamlit as st
-from streamlit_chat import message
+from langchain import OpenAI
+
+st.title('🦜🔗 Langchain Quickstart App')
 
 with st.sidebar:
-    openai_api_key = st.text_input('OpenAI API Key',key='chatbot_api_key')
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+  openai_api_key = st.text_input('OpenAI API Key')
 
-st.title("💬 Streamlit GPT")
-#openai.api_key = st.secrets.openai_api_key
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
-with st.form("chat_input", clear_on_submit=True):
-    a, b = st.columns([4, 1])
-    user_input = a.text_input(
-        label="Your message:",
-        placeholder="What would you like to say?",
-        label_visibility="collapsed",
-    )
-    b.form_submit_button("Send", use_container_width=True)
-
-for msg in st.session_state.messages:
-    message(msg["content"], is_user=msg["role"] == "user")
-
-if user_input and not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.")
-    
-if user_input and openai_api_key:
-    openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": user_input})
-    message(user_input, is_user=True)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    message(msg.content)
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are 3 key advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if submitted:
+    generate_response(text)
