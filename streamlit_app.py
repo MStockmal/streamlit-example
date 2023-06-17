3d49e497bc61efbdcdc9ab118beebc5a9865c074
Matt Stockmal <mestockmal@gmail.com>
2023-06-17 01:33:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ pinecone.init(
     api_key=PINECONE_API_KEY,  # find at app.pinecone.io
     environment=PINECONE_API_ENV  # next to api key in console
 )
-index_name = "hinsdale-doings-try1" # put in the name of your pinecone index here
+index_name = "hinsdale-history-try2" # put in the name of your pinecone index here
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 query = "Who was William Robbins? Please write 3 paragraphs explaining his impact on the development of Hinsdale"
@@ -33,7 +33,7 @@ def generate_response(input_text):
     llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name = 'gpt-3.5-turbo')
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     docsearch = Pinecone.from_existing_index(index_name, embeddings)
-    docs = docsearch.similarity_search(input_text, k=6)
+    docs = docsearch.similarity_search(input_text, k=10)
     chain = load_qa_chain(llm, chain_type="stuff")
     st.info(chain.run(input_documents=docs, question=input_text))
     # chain = load_summarize_chain(llm, chain_type="map_reduce")
@@ -44,7 +44,7 @@ def generate_response(input_text):
 
 
 with st.form('my_form'):
-  text = st.text_area('Ask a Hinsdale hisotry question:', 'Who was William Robbins? Explain his impact on the development of Hinsdale')
+  text = st.text_area('Ask a Hinsdale history question:', 'Who was William Robbins? Explain his impact on the development of Hinsdale')
   submitted = st.form_submit_button('Submit')
   if submitted:
     generate_response(text)
