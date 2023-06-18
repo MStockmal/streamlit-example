3e9c10ab9ea1c0fbaa3e34ca7e47b275f116590d
Matt Stockmal <mestockmal@gmail.com>
2023-06-17 23:27:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ pinecone.init(
     api_key=PINECONE_API_KEY,  # find at app.pinecone.io
     environment=PINECONE_API_ENV  # next to api key in console
 )
-index_name = "hinsdale-history-try2" # put in the name of your pinecone index here
+index_name = "hinsdale-history-try3" # put in the name of your pinecone index here
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 query = "Who was William Robbins? Please write 3 paragraphs explaining his impact on the development of Hinsdale"
@@ -33,7 +33,7 @@ def generate_response(input_text):
     llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name = 'gpt-3.5-turbo')
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     docsearch = Pinecone.from_existing_index(index_name, embeddings)
-    docs = docsearch.max_marginal_relevance_search(input_text, k=10)
+    docs = docsearch.similarity_search(input_text, k=4)
     chain = load_qa_chain(llm, chain_type="stuff")
     st.info(chain.run(input_documents=docs, question=input_text))
     # chain = load_summarize_chain(llm, chain_type="map_reduce")
